2b74f859e274c5342693a996648db16e05ef367e
Saifi Ahmada <saifiahmada@gmail.com>
2013-04-15 10:41:21 +0800

    commit

---
+++ ProgramHati/src/org/programhati/belajar/Hacker.java
@@ -0,0 +1,48 @@
+package org.programhati.belajar;
+
+/** @author Saifi Ahmada Mar 28, 2013 10:50:08 AM  **/
+
+public class Hacker {
+	
+	private String idHacker;
+	
+	private String nama;
+	
+	private boolean happy;
+	
+	private boolean sad;
+
+	public String getIdHacker() {
+		return idHacker;
+	}
+
+	public void setIdHacker(String idHacker) {
+		this.idHacker = idHacker;
+	}
+
+	public String getNama() {
+		return nama;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+
+	public boolean isHappy() {
+		return happy;
+	}
+
+	public void setHappy(boolean happy) {
+		this.happy = happy;
+	}
+
+	public boolean isSad() {
+		return sad;
+	}
+
+	public void setSad(boolean sad) {
+		this.sad = sad;
+	} 
+	
+}
+
---
+++ ProgramHati/src/org/programhati/belajar/IfHacker.java
@@ -0,0 +1,20 @@
+package org.programhati.belajar;
+
+/** @author Saifi Ahmada Mar 28, 2013 10:51:47 AM  **/
+
+public class IfHacker {
+	
+	private static Hacker hacker;
+	
+	public static void main(String[] args) {
+		
+		if (hacker.isHappy()){
+			System.out.println(" hack your bank account ");
+		} else {
+			System.out.println(" hack your girl friend ");
+			System.out.println(" he he he :D ");
+		}
+	}
+		
+}
+
